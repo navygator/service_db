eae88e57f7515dd2653dd0842eb8a9a7603054d4
Alexey Nureev <navygator@myopera.com>
2012-11-07 17:17:15 +0400

    Some refactoring

--- ServiceDB/Models/RequestItemPartModel.cs
+++ ServiceDB/Models/RequestItemPartModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
 
 namespace ServiceDB.Models
 {
@@ -14,6 +16,70 @@ namespace ServiceDB.Models
 			if (property != "Updated_at")
 				Updated_at = DateTime.Now;
 			base.OnPropertyChanged(property);
+		}
+		
+		private static EntityDataModelContainer db = new EntityDataModelContainer();
+
+		public static List<RequestItemPart> All()
+		{
+			return db.RequestItemParts.ToList();
+		}
+
+		public static RequestItemPart Find(int id)
+		{
+			return db.RequestItemParts.Single(e => e.Id == id);
+		}
+
+		public static bool Create(RequestItemPart requestItemPart)
+		{
+			bool result = true;
+			try
+			{
+				db.RequestItemParts.AddObject(requestItemPart);
+				db.SaveChanges();
+			}
+			catch (Exception)
+			{
+				result = false;
+			}
+
+			return result;
+		}
+
+		public static bool Update(RequestItemPart requestItemPart)
+		{
+			bool result = true;
+			try
+			{
+				EntityDataModelContainer mc = new EntityDataModelContainer();
+				mc.RequestItemParts.Attach(requestItemPart);
+				mc.ObjectStateManager.ChangeObjectState(requestItemPart, EntityState.Modified);
+				mc.SaveChanges();
+				db.Refresh(System.Data.Objects.RefreshMode.StoreWins, requestItemPart);
+			}
+			catch (Exception)
+			{
+				result = false;
+			}
+
+			return result;
+		}
+
+		public static bool Destroy(int id)
+		{
+			bool result = true;
+			try
+			{
+				var requestItemPart = Find(id);
+				db.RequestItemParts.DeleteObject(requestItemPart);
+				db.SaveChanges();
+			}
+			catch (Exception)
+			{
+				result = false;
+			}
+
+			return result;
 		}
 
 		public class RequestItemPartMetaData
